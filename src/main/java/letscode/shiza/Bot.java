18277f4e686099fd267576655d6187cd5686e509
Martizik <con_tom@mail.ru>
2022-09-02 17:13:09 +0400

    **

---
+++ src/main/java/letscode/shiza/Bot.java
@@ -0,0 +1,33 @@
+package letscode.shiza;
+
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+
+
+public class Bot {
+    private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+
+    public void serve() {
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(this::process);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+
+
+    }
+
+    private void process(Update update) {
+        Message message = update.message();
+        CallbackQuery callbackQuery = update.callbackQuery();
+
+        long chatId = update.message().chat().id();
+
+
+       bot.execute(new SendMessage(chatId, "Hello!"));
+    }
+}
