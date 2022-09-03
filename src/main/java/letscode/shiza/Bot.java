5d64a97da6ee044044ebe4b0d9ee77e6f96e7580
Martizik <con_tom@mail.ru>
2022-09-03 16:06:14 +0400

    **

--- src/main/java/letscode/shiza/Bot.java
+++ src/main/java/letscode/shiza/Bot.java
@@ -12,13 +12,14 @@ import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.model.request.InlineQueryResultArticle;
 import com.pengrad.telegrambot.request.AnswerInlineQuery;
 import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
-
 import java.awt.event.ItemListener;
 
 
 public class Bot {
     private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+    private final String WAIT_LABEL = "Wait...";
 
     public void serve() {
         bot.setUpdatesListener(updates -> {
@@ -36,21 +37,67 @@ public class Bot {
 
         BaseRequest request = null;
 
-        if (inlineQuery != null){
-            InlineQueryResultArticle stone = bildInlineButton("stone","Stone","0");
-            InlineQueryResultArticle scissor = bildInlineButton("scissor","Scissor","1");
-            InlineQueryResultArticle paper = bildInlineButton("paper","Paper","2");
+        if (message != null && message.viaBot() != null && message.viaBot().username().equals("@testttGame_bot")) {
+            InlineKeyboardMarkup replyMarkup = message.replyMarkup();
+            if (replyMarkup == null){
+                return;
+            }
+            InlineKeyboardButton[][] buttons = replyMarkup.inlineKeyboard();
 
-            request= new AnswerInlineQuery(inlineQuery.id(),stone,scissor,paper);
+            if (buttons == null){
+                return;
+            }
 
+            InlineKeyboardButton button = buttons[0][0];
+            String buttonLabel = button.text();
+
+            if(!buttonLabel.equals(WAIT_LABEL)){
+                return;
+            }
+
+            Long chatId = message.chat().id();
+            String senderName = message.from().firstName();
+            String senderChose = button.callbackData();
+            Integer messageId = message.messageId();
+            request = new EditMessageText(chatId, messageId, message.text())
+                    .replyMarkup(
+                            new InlineKeyboardMarkup(
+                                    new InlineKeyboardButton("\uD83E\uDEA8")
+                                            .callbackData(String.format("%d %s %s %s", chatId, senderName, senderChose, "0")),
+                                    new InlineKeyboardButton("✂ ")
+                                            .callbackData(String.format("%d %s %s %s", chatId, senderName, senderChose, "1")),
+                                    new InlineKeyboardButton("\uD83D\uDCF0")
+                                            .callbackData(String.format("%d %s %s %s", chatId, senderName, senderChose, "2"))
+
+                            )
+                    );
+
+
+        } else if (inlineQuery != null){
+            InlineQueryResultArticle stone = bildInlineButton("stone","\uD83E\uDEA8 Stone","0");
+            InlineQueryResultArticle scissor = bildInlineButton("scissor","✂ Scissor","1");
+            InlineQueryResultArticle paper = bildInlineButton("paper","\uD83D\uDCF0 Paper","2");
+
+            request = new AnswerInlineQuery(inlineQuery.id(),stone,scissor,paper).cacheTime(1);
+
+        } else if (callbackQuery != null) {
+            String[] data = callbackQuery.data().split(" ");
+            String chatId = data[0];
+            String senderName = data[1];
+            String senderChose = data[2];
+            String opponentChose = data[3];
+            String opponentName = callbackQuery.from().firstName();
+            System.out.println("");
+            
         }
 
-        else if(message != null) {
-            long chatId = update.message().chat().id();
-            request = new SendMessage(chatId,"Shalom");
-        }
 
-        if (request != null) {
+        //else if(message != null) {
+        //    long chatId = update.message().chat().id();
+        //    request = new SendMessage(chatId,"Shalom");
+        //}
+
+         if (request != null) {
             bot.execute(request);
         }
     }
@@ -59,7 +106,7 @@ public class Bot {
         return new InlineQueryResultArticle(id, title, "I m ready to fight")
                 .replyMarkup(
                         new InlineKeyboardMarkup(
-                                new InlineKeyboardButton("Wait...").callbackData("0"))
+                                new InlineKeyboardButton(WAIT_LABEL).callbackData("0"))
                 );
     }
 }
